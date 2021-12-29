ee6604e5a82bcfbc780463c00da19589cc746eff
Sirius <curiosity@Siriuss-MacBook-Pro.local>
2021-12-29 12:55:00 +0530

    added a Frame onto the main window

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ H=400
 
 def paint(e):
 
+    # Brush Parameters
+    brush_width = 20
+    brush_color = "green"
+    brush_type = ROUND
+
     x1 = e.x - 1
     y1 = e.y - 1
 
     x2 = e.x + 1
     y2 = e.y + 1
 
-    my_canvas.create_line(x1, y1, x2, y2, fill='red', smooth=True)
+    my_canvas.create_line(x1, y1, x2, y2, fill=brush_color, smooth=True, width=brush_width, capstyle=brush_type)
 
 # create window
 root = Tk()
@@ -27,6 +32,10 @@ my_canvas.pack(pady=20)
 
 #bind the mouse event of Left click button press mouse motion with Canvas
 my_canvas.bind('<B1-Motion>', paint)
+
+# Create Brush options Frame
+brush_options_frame = Frame(root)
+brush_options_frame.pack(pady=20)
 
 
 
