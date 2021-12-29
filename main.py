d1f99070a3beb252b8b4ec61943a6383d22f0343
Sirius <curiosity@Siriuss-MacBook-Pro.local>
2021-12-29 12:42:13 +0530

    added the mouse event with the canvas to paint on the canvas

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from tkinter import *
 
-
+# Canvas Width and Height
 W=600
 H=400
 
+def paint(e):
+
+    x1 = e.x - 1
+    y1 = e.y - 1
+
+    x2 = e.x + 1
+    y2 = e.y + 1
+
+    my_canvas.create_line(x1, y1, x2, y2, fill='red', smooth=True)
 
 # create window
 root = Tk()
@@ -12,7 +21,18 @@ root.geometry("800x800")
 
 # create a Canvas widget
 my_canvas = Canvas(root, width=W, height=H, bg="white")
+
+# put the Canvas on the main window
 my_canvas.pack(pady=20)
+
+#bind the mouse event of Left click button press mouse motion with Canvas
+my_canvas.bind('<B1-Motion>', paint)
+
+
+
+
+
+
 
 
 
